6853a4eebd3c9e9e50b2628decf6b7e7dee3eaba
Rostyslav Ivasiv <rivasiv@gmail.com>
2016-12-22 21:34:41 -0800

    Init git scripts commit. Added git user mail/user configs.

---
+++ git/gitGlobalConfig.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git config --global user.email "rivasiv@gmail.com"
+git config --global user.name "Rostyslav Ivasiv"
---
+++ git/gitLocalConfig.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git config user.email "rivasiv@gmail.com"
+git config user.name "Rostyslav Ivasiv"
